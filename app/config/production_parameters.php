e17a9a79c1c7023452561397e2f0136cb529f59e
Pablo Díaz Márquez <pablo5_diaz@hotmail.com>
2016-03-11 17:21:26 -0600

    update production parameters

--- app/config/production_parameters.php
+++ app/config/production_parameters.php
@@ -1,5 +1,11 @@
 <?php
     //$db = parse_url(getenv('CLEARDB_DATABASE_URL'));
+    $container->setParameter('database_driver', 'pdo_mysql');
+    $container->setParameter('database_host', getenv('DB_HOST'));
+    $container->setParameter('database_port', 3306);
+    $container->setParameter('database_name', getenv('DB_NAME'));
+    $container->setParameter('database_user', getenv('DB_USERNAME'));
+    $container->setParameter('database_password', getenv('DB_PASSWORD'));
     $container->setParameter('secret', getenv('SECRET'));
     $container->setParameter('locale', 'es');
     $container->setParameter('mailer_transport', gmail);
